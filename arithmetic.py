90f83a9462f465a762117556f56c6c9b6820a19e
vagrant <vagrant@vagrant.vm>
2021-03-14 00:57:11 +0000

    completed square function

--- arithmetic.py
+++ arithmetic.py
@@ -2,8 +2,9 @@
 
 
 def add(num1, num2):
-    """Return the sum of the two inputs.""" 
+    """Return the sum of the two inputs."""
     return num1 + num2
+
 
 
 def subtract(num1, num2):
@@ -18,10 +19,12 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
+    return num1 / num2
 
 
 def square(num1):
     """Return the square of the input."""
+    return num1 * num1
 
 
 def cube(num1):
